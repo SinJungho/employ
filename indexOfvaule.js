0b584c987604c1ce86750ae594b6511d6d8f4cd6
SinJungho <zmtls0516@naver.com>
2021-04-27 16:19:35 +0900

    first commit

---
+++ indexOfvaule.js
@@ -0,0 +1,9 @@
+function counter(){
+    let content = document.getElementById("jasoseo").value;
+    if(content.length > 200){
+        content = content.substring(0, 200);
+        document.getElementById("jasoseo").value = content;
+    }
+    document.getElementById("count").innerHTML = '(' + content.length + '/200)';
+}
+counter();
